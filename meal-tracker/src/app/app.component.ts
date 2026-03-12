ca4114181856dbc1ae55d0dfc3e6a6b7217b224e
KJ OBrien <kjokjo219@gmail.com>
2026-03-11 21:56:01 -0400

    adds nav bar for buttons

--- meal-tracker/src/app/app.component.ts
+++ meal-tracker/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MealsComponent } from './meals/meals.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MealsComponent],
+  imports: [RouterOutlet, MealsComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
