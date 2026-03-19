504b9cf4d62fc63612cc8d51f6ee291160bd700a
KJ OBrien <kjokjo219@gmail.com>
2026-03-18 21:45:28 -0400

    adds better styling

--- meal-tracker/src/app/food/food.component.ts
+++ meal-tracker/src/app/food/food.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-food',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './food.component.html',
   styleUrl: './food.component.css',
   standalone: true
 })
 export class FoodComponent {
+
+  protein = 22;
+  carbs = 1;
+  fats = 9;
+
+  pieChartType: ChartType = "pie";
+
+  pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          font: {
+            size: 10
+          }
+        }
+      }
+    }
+  };
+
+  pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: ['Protein', 'Carbs', 'Fats'],
+    datasets: [
+      {
+        data: [this.protein, this.carbs, this.fats],
+        backgroundColor: ['#A9CAB0', '#FFCE56', '#FF6384']
+      }
+    ]
+  };
 
 }
