d8c516c16b237a138de490fc4a7757ca5010b8f4
KJ OBrien <kjokjo219@gmail.com>
2026-03-18 20:07:22 -0400

    adds shell of new comp

---
+++ meal-tracker/src/app/new-meal/new-meal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-meal',
+  imports: [],
+  templateUrl: './new-meal.component.html',
+  styleUrl: './new-meal.component.css'
+})
+export class NewMealComponent {
+
+}
